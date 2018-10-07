56b36c3ac5011f1f83ab58a7b2ff8f18d5444aef
Wade Spires <w.h.spires@gmail.com>
2018-10-06 22:00:13 -0400

    Add trie

--- binary_search/main.cpp
+++ binary_search/main.cpp
@@ -44,6 +44,47 @@ std::size_t find(std::vector<int> const & array, int key)
     return lower - std::cbegin(array);
 }
 
+std::size_t find_leftmost(std::vector<int> const & array, int key)
+{
+    auto lower = std::cbegin(array);
+    auto upper = std::cend(array) - 1;
+    while (lower < upper and *lower != key)
+    {
+        auto mid = lower + (upper - lower) / 2;
+        if (key <= *mid)
+        {
+            upper = mid;
+        }
+        else
+        {
+            lower = mid + 1;
+        }
+    }
+    return lower - std::cbegin(array);
+}
+
+std::size_t find_rightmost(std::vector<int> const & array, int key)
+{
+    auto lower = std::cbegin(array);
+    auto upper = std::cend(array) - 1;
+    //while (lower < upper and *upper != key)
+    while (lower < upper and *lower != key)
+    {
+        auto mid = lower + (upper - lower) / 2;
+        if (key < *mid)
+        {
+            //upper = mid - 1;
+            upper = mid;
+        }
+        else
+        {
+            //lower = mid;
+            lower = mid + 1;
+        }
+    }
+    return lower - std::cbegin(array);
+}
+
 void print(std::vector<int> const & array)
 {
     std::cout << "[";
@@ -56,18 +97,22 @@ void print(std::vector<int> const & array)
 
 void print(std::vector<int> const & array, int key)
 {
-    auto idx = find(array, key);
+    //auto idx = find(array, key);
+    //auto idx = find_leftmost(array, key);
+    auto idx = find_rightmost(array, key);
     print(array);
     std::cout << ": key=" << key << ", idx=" << idx << ", value=" << array[idx] << ", found=" << (array[idx] == key) << std::endl;
 }
 
 void test_binary_search()
 {
-    print({1, 2, 3}, 1);
-    print({1, 2, 3}, 2);
-    print({1, 2, 3}, 3);
-    print({1, 2, 3}, 0);
-    print({1, 2, 3}, 4);
+    //auto array = {1, 2, 3};
+    auto array = {1, 2, 2, 3};
+    print(array, 1);
+    print(array, 2);
+    print(array, 3);
+    print(array, 0);
+    print(array, 4);
 }
 
 int main(int argc, char * argv[])
